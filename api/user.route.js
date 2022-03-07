d4b874ec464d12e07c9636fe0b4aeccbfe13a51b
czekk <czerintonkr@zoho.com>
2022-03-07 16:43:53 +0530

    9th commit

--- api/user.route.js
+++ api/user.route.js
@@ -6,7 +6,7 @@ router.route('/').post(UserController.apiGetUser);
 router.route('/authenticate').post(UserController.apiAuthenticate);
 router.route('/register').post(UserController.apiCreateUser);
 router.route('/:id').put(UserController.apiUpdateUser);
-// router.route('/:id').get(UserController.apiGetUserById)
+router.route('/:id').get(UserController.apiGetUserById)
 router.route('/:id').delete(UserController.apiDeleteUser)
 
 export default router;
